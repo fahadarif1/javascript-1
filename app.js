a7d772625901db3f74d75ab02973d04473118473
fahadarif1 <134074010+fahadarif1@users.noreply.github.com>
2024-03-04 09:24:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+// alert("welcome to salt n pappar ")
+
+
+// var num1 = 6
+// var num2 = 3
+                  
+// var result = num1++ + ++num2 + ++num1 + ++num2
+
+// console.log(result);
+
+
+
+
+// var totalcost = "23" + "23"
+// console.log(totalcost);
+
+
+
+// var username = prompt("enter your name"+)
+// console.log(username);
+
+// Task 1
+let num = 10;
+if(num > 0){
+  console.log(`${num} is Greater Then 0`);
+}
+
+// Task 2
+let number = 5;
+let number2 = 10;
+if (number == number2){
+  console.log(`${number} equal to ${number2}`)
+} else {
+  console.log(`${number} is not equal to ${number2}`)
+}
+
+// Task 3
+let userInput = +prompt("Enter Any Number")
+if (userInput % 2 == 0){
+  console.log(`The Number ${userInput} is Even`);
+} else {
+  console.log(`The Number ${userInput} is Odd`);
+} 
+// Task 4
+let a = 10;
+let b = 15;
+if (a < b){
+  console.log("a is greater");
+} else {
+  console.log("b is greater");
+}
